230188dc41253de032173f2490aca6c0db89ebaf
38071 <makster559553@gmail.com>
2020-04-26 15:34:22 +0300

    added medis section and styled it on mobiles

---
+++ app/js/component/media-slider.js
@@ -0,0 +1,35 @@
+(function ($, window, document) {
+  function mediaSlider() {
+    let slider = {
+      container: $(".js_media-body--slider"),
+      scroll: $(".js_media-body--scroll"),
+      item: $(".js_media-slider--block"),
+      item_width: $(".js_media-slider--block").width(),
+      item_length: $(".js_media-slider--block").length,
+      margin: 10,
+    };
+
+    let { container, scroll, item, item_width, item_length, margin } = slider;
+
+    let $media_slider_width =
+      item_width * item_length + margin * (item_length - 1);
+
+    container.css({
+      width: $media_slider_width + "px",
+    });
+
+    scroll.attr("max", $media_slider_width);
+
+    scroll.on("input change", function () {
+      let $this = $(this);
+
+      container.css({
+        transform: "translateX(-" + $this.val() + "px)",
+      });
+    });
+  }
+
+  $(() => {
+    mediaSlider();
+  });
+})(window.jQuery, window, document);
--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,6 +1,40 @@
 "use strict";
 
 (function ($, window, document) {
+  function mediaSlider() {
+    var slider = {
+      container: $(".js_media-body--slider"),
+      scroll: $(".js_media-body--scroll"),
+      item: $(".js_media-slider--block"),
+      item_width: $(".js_media-slider--block").width(),
+      item_length: $(".js_media-slider--block").length,
+      margin: 10
+    };
+    var container = slider.container,
+        scroll = slider.scroll,
+        item = slider.item,
+        item_width = slider.item_width,
+        item_length = slider.item_length,
+        margin = slider.margin;
+    var $media_slider_width = item_width * item_length + margin * (item_length - 1);
+    container.css({
+      width: $media_slider_width + "px"
+    });
+    scroll.attr("max", $media_slider_width);
+    scroll.on("input change", function () {
+      var $this = $(this);
+      container.css({
+        transform: "translateX(-" + $this.val() + "px)"
+      });
+    });
+  }
+
+  $(function () {
+    mediaSlider();
+  });
+})(window.jQuery, window, document);
+
+(function ($, window, document) {
   function teamSlider() {
     var slider = {
       container: $(".js_team-body--slider"),
